10eaa523355a8cfe0137571b046fd41c4b6c1394
Hari-Bombon <haripriya14022003@gmail.com>
2023-10-09 20:43:34 +0530

    Added the header

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const HeroSection = () => {
   return (
     <section>
-      <h1 className='text-white'>Portfolio</h1>
+      <div className='grid grid-cols-1 lg:grid-cols-12'>
+        <div className='col-span-7'>
+      <h1 className='text-white mb-4 lg:text-4xl font-extrabold'>
+        Hello! I am Haripriya
+        </h1>
+      <p className='text-[#ADB7BE] text-lg lg:text-xl'>
+        Currently pursuing B.E CSE
+        </p>
+        </div>
+        </div>
     </section>
   );
 };
