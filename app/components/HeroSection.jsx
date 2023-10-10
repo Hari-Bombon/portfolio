bcb685ac8612f96cfbaa71e97e2b4f720af217b7
Hari-Bombon <haripriya14022003@gmail.com>
2023-10-10 20:07:28 +0530

    added the button

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -9,12 +9,16 @@ const HeroSection = () => {
       <h1 className='text-white mb-4 lg:text-4xl font-extrabold'>
         Hello! I am Haripriya
         </h1>
-      <p className='text-[#ADB7BE] text-lg lg:text-xl'>
+      <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>
         Im a bd, fd
         </p>
+        <div>
+          <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Hire Me</button>
+          <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download CV</button>
         </div>
-        <div className='col-span-5'>
-          <div className='rounded-full bg-[#181818] '>
+        </div>
+        <div className='col-span-5 place-self-center mt-4 ;g'>
+          <div className='rounded-full bg-[#181818] w-[250px] h-[250]'>
           <image
           src="./images/profile.jpeg"
           alt="hero-image"
