8a35497b491a92cc60953035186a4f693b07fa33
Hari-Bombon <haripriya14022003@gmail.com>
2023-10-12 20:44:41 +0530

    Added the gradient color to the btn and header

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -7,14 +7,19 @@ const HeroSection = () => {
       <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-self-center text-center sm:text-left'>
       <h1 className='text-white mb-4 sm:text-5xl lg:text-4xl font-extrabold'>
-        Hello! I am Haripriya
+       <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-400'> 
+       Hello! I am {""}
+       </span>
+        Haripriya
         </h1>
-      <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>
+      <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
         Im a bd, fd
         </p>
         <div>
-          <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Hire Me</button>
-          <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>Download CV</button>
+          <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-200 via-sky-500 to-pink-500 hover:bg-slate-200 text-white'>Hire Me</button>
+          <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:big-slate-800 hover:bg-slate-800 text-white border border-white mt-3'>
+            <span>Download CV </span>
+            </button>
         </div>
         </div>
         <div className='col-span-5 place-self-center mt-4 ;g'>
