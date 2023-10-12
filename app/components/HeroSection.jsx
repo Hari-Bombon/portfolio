1b9eda472e06591c78dabe1e6abab8671783f82a
Hari-Bombon <haripriya14022003@gmail.com>
2023-10-12 20:55:03 +0530

    Added the typeanimation

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+
+
+   
 
 
 const HeroSection = () => {
@@ -10,15 +14,32 @@ const HeroSection = () => {
        <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-400'> 
        Hello! I am {""}
        </span>
-        Haripriya
+       <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Haripriya',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Web developer',
+        1000,
+        'Ethereum developer',
+        1000,
+        'Front-end developer',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '2em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
+        
         </h1>
       <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
         Im a bd, fd
         </p>
         <div>
           <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-200 via-sky-500 to-pink-500 hover:bg-slate-200 text-white'>Hire Me</button>
-          <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:big-slate-800 hover:bg-slate-800 text-white border border-white mt-3'>
-            <span>Download CV </span>
+          <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:big-slate-800 hover:bg-slate-800 text-white mt-3'>
+            <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV </span>
             </button>
         </div>
         </div>
