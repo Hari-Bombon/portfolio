0a49b42a0499105999c0991bab07745c70404dc3
Hari-Bombon <haripriya14022003@gmail.com>
2023-10-15 20:17:07 +0530

    Added the email section

---
+++ app/components/EmailSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const EmailSection = () => {
+  return (
+    <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
+        <div> 
+            <h5 className='text-xl font-bold text-white my-2'>Letz Connect</h5>
+        <p className='text-[#ADB7BE] mb-4 max-w-md'>
+            {""}
+            I am Currently looking for new opportunities, my inbox is always open, 
+            Whether you have a question or just want to say hi , I will try my best to get back to you!
+        </p>
+        <div className='socials flex flex-row gap-2'></div>
+        </div>
+        </section>
+  );
+}
+
+export default EmailSection
--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
+
 const ProjectCard = ({imgUrl , title , description}) => {
   return (
-    <div><div className="h-52 md:h-72"
+    <div><div className="h-52 md:h-72 rounded-t-xl relative"
      style={{ background: `url(${imgUrl})`, backgroundSize:"cover"}}
      ></div>
-     <div className='text-white'>
-        <h5>{title}</h5>
-        <p>{description}</p>
+
+     <div className='overlay absolute top-0 left-0 w-full h-full bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500'></div>
+     <div className='text-white rounded-b-xl bg-[#181818] py-6 px-4'>
+        <h5 className='text-xl font-semibold mb-2'>{title}</h5>
+        <p className='text-[#ADB7BE]'>{description}</p>
      </div>
      </div>
   )
