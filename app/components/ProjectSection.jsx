5a2350b1d9ff41ad115e5fc2dc1367b5db6be3a1
Hari-Bombon <haripriya14022003@gmail.com>
2023-10-15 19:48:21 +0530

    Added the project details

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
 
-const ProjectSection = () => {
-  return (
-    <h2>My Projects</h2>
-  )
+const ProjectData = [
+    {
+        id:1,
+        title: "Food Website",
+        description: "",
+        images: "/images/projects/food.png",
+        tag:["All", "Web"],
+    },
+
+    {
+        id:2,
+        title: "Car Animation",
+        description: "",
+        images: "/images/projects/car.png",
+        tag:["All", "Web"],
+    },
+
+    {
+        id:3,
+        title: "Login/Sign-up",
+        description: "",
+        images: "/images/projects/ls.png",
+        tag:["All", "Web"],
+    },
+
+    {
+        id:4,
+        title: "Random-Quote Generator",
+        description: "",
+        images: "/images/projects/qp.png",
+        tag:["All", "Web"],
+    },
+];
+
+const ProjectSection =() =>{
+    return(
+        <>
+        <h2>My Projects</h2>
+        <div>{ProjectData.map((project) => <ProjectCard key ={project.id} title={project.title} description={project.description} imgUrl={project.image} />)}</div>
+        </>
+    )
 }
 
 export default ProjectSection
