b2d9a9fca09e98526d87276b101242aa8e4aa515
Hari-Bombon <haripriya14022003@gmail.com>
2023-10-17 19:56:31 +0530

    Added the email fn

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -7,6 +7,28 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 const EmailSection = () => {
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        const data = {
+            email:e.target.email.value,
+            subject: e.target.subject.value,
+            message:e.target.message.value,
+        }
+        const JSONdata = JSON.stringify(data);
+        const endpoint = "app/send";
+        //form the req for sending data to server.
+        const options = {
+            //the method is POST bcoz we are sending data
+            method: 'POST',
+            //tell the server we're sending json
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            //body of the request is the json data we created above
+            body:JSONdata,
+        }
+    }
   return (
     <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'>
         <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-x-1/2'></div>
--- app/send/route.js
+++ app/send/route.js
@@ -4,15 +4,20 @@ import {Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(){
+export async function POST(req, res){
+    const {body} = req;
+    const {email , subject , message} =  body;
     try{
         const data = await resend.emails.send({
             from: "fromEmail",
-            to:[""],
+            to:["haripriya14022003@gmail.com"],
             subject:"Hello world",
             react: (
                 <>
-                <p>Email Body</p>
+                <h1>{subject}</h1>
+                <p>Thank you for Contacting us!</p>
+                <p>New message submitted:</p>
+                <p>{message}</p>
                 </>
             ),
         });
