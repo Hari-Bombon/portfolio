87abaf2a658eaeb765ed2fd6cd91bd56946b2cd6
Hari-Bombon <haripriya14022003@gmail.com>
2023-10-26 13:55:00 +0530

    debugged

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -1,16 +1,19 @@
-"use Client"
-import React , {usestate , useRef} from 'react'
-import ProjectCard from './ProjectCard'
+
+"use client"
+import { useInView } from 'framer-motion';
+import { useRef, useState } from 'react';
+import ProjectCard from './ProjectCard';
 import ProjectTag from './ProjectTag';
-import {motion , useInView} from "framer-motion"
+
+
 
 const ProjectData = [
 
     {
         id:1,
         title: "Food Website",
-        description: "",
-        images: "/images/projects/food.png",
+        description: "hello",
+        images: "https://blogger.googleusercontent.com/img/a/AVvXsEh3Ch31LRZeg83imAPCiwTcVshG4WbB6O3Ua_olRwrIP1pJepWecNvREHk1m6APUzh9Qr-Eu0I3IOlyLUbwPdEVseEE8b7ZJw8KdOtLEBf_DTLugBcN5_qx7bHj4OXlAQoILOKu-3NIaMUgQCoYqQvE3kkGYRLUk_ZVZv5rYd1OcFsgXfp4dMpWEWMdiFg",
         tag:["All", "Web"],
         gitUrl:"/",
         previewUrl:"/",
@@ -19,8 +22,8 @@ const ProjectData = [
     {
         id:2,
         title: "Car Animation",
-        description: "",
-        images: "/images/projects/car.png",
+        description: "hello",
+        images: "https://blogger.googleusercontent.com/img/a/AVvXsEgvuymyumd-Tik900AjZzn2uaqd44c5Pvl23AMITe3ZbeDL71DKIkAQ8fRz6kCfMzhsJ2LvOy9Sat3peIM6Ek-cH3zcTBV4EQZH1dd-reelbWgQDADn4d9ibCMtqwy80Z-ifzlFM7WhYm01qV9O41jh629Gzi0PLOuwZEFEjM4hxmEPKvK6JfzquIv5aO4",
         tag:["All", "Web"],
         gitUrl:"/",
         previewUrl:"/",
@@ -29,8 +32,8 @@ const ProjectData = [
     {
         id:3,
         title: "Login/Sign-up",
-        description: "",
-        images: "/images/projects/ls.png",
+        description: "hello",
+        images: "https://blogger.googleusercontent.com/img/a/AVvXsEgohIJWx58KrlWnJrRhUfiyGUIXfdHvvdq_aR9jAgWi0ZAjTAFZjhhdDXerUGMxuUgRJqcZt6k4fsSu7ogKqRtZJCuKAw5kuS-UPP9yJZKzNrpj9F4yLgBbxbYioXyW5v0vft7lgn7IKdW1plZn_cHojOtUnGZQ5IZVEMBrbGZoyt_BaQzxfJviy85XeDk",
         tag:["All", "Web"],
         gitUrl:"/",
         previewUrl:"/",
@@ -39,27 +42,23 @@ const ProjectData = [
     {
         id:4,
         title: "Random-Quote Generator",
-        description: "",
-        images: "/images/projects/qp.png",
+        description: "hello",
+        images: "https://blogger.googleusercontent.com/img/a/AVvXsEjdZI8oYYQeh83215DWyX7a5FA3JhKN4BGJFhV9Us_USMp897ESel7GxJuvzWAOwfZfxTGQ7QeGzKmtGPCX1mFPkY8wO4EbvNNoIm8TKr2cPMD-oaEOZBHQhnalKAM_Pilktg1nxpR34U23qiWj8p_s_EP1FqOAVb8sPrn1YMMqIDG-lG4fLMQn0ezMOdo",
         tag:["All", "Web"],
         gitUrl:"/",
         previewUrl:"/",
-    },
+    }
 ];
 
 const ProjectSection =() =>{
-    const [tag , setTag] = usestate("All");
+    const [tag , setTag] = useState("All");
     const ref = useRef(null);
     const isInView = useInView(ref, {once: true});
 
     const handleTagChange = (newTag) => {
         setTag(newTag);
     };
-
-    const filteredProjects = projectsData.filter((project) =>{
-        project.tag.includes(tag)
-    });
-    return(
+  return(
         <section ref ={ref}>
         <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
             My Projects
@@ -86,12 +85,12 @@ const ProjectSection =() =>{
         </div>
  
         <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
-    {filteredProjects.map((project) => (
+    {ProjectData.map((project) => (
         <ProjectCard
             key={project.id}
             title={project.title}
             description={project.description}
-            imgUrl={project.image}
+            imgUrl={project.images}
             gitUrl={project.gitUrl}
             preview={project.preview}
         />
