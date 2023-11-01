e8dd13c9bd1e4bfba6b3977463471612acdc291f
Hari-Bombon <haripriya14022003@gmail.com>
2023-11-01 09:20:41 +0530

    debugged

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -6,7 +6,9 @@ const ProjectCard = ({imgUrl , title , description}) => {
      
      <div>
      <div className=' h-5 md:h-72 text-white rounded-t-xl relative'>
-    style={{ background: `url(${imgUrl})`, backgroundSize:"cover"}}
+ 
+
+   <img src={imgUrl} alt="" className=" bg-cover h-52 w-full"   />
     </div>
     <div className="overlay absolute top-0 left-0 w-full h-full bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500"> </div>
         <h5 className='text-xl font-semibold mb-2'>{title}</h5>
