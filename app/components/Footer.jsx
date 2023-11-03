fdd374e0a13d0b23851136d15dfb0a4c67790626
Hari-Bombon <haripriya14022003@gmail.com>
2023-11-03 14:11:43 +0530

    updated

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className='footer border border-t-[#33353F] z-10 border-l-transparent border-r-transparent text-white'>
         <div className='container p-12 flex justify-between'>
             <span>HD</span>
-            <p className='text-slate-800'>All rights reserved</p>
+            <p className='text-violet-800'>All rights reserved</p>
         </div>
         </footer>
   )
